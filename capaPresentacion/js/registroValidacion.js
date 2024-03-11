00f0e80d94d966e78df5f1316f2c68ffc7c6a3db
Marta <marta.alarconalcolea@gmail.com>
2024-03-11 09:22:33 +0100

    Primera parte del registro

---
+++ capaPresentacion/js/registroValidacion.js
@@ -0,0 +1,23 @@
+let enviar = document.getElementById('boton');
+let contraseña = document.getElementById('password');
+
+contraseña.addEventListener('input', function() {
+    let valor = contraseña.value;
+
+    
+})
+
+
+enviar.addEventListener('click', function(evento) {
+    evento.preventDefault();
+
+    let nombre = document.getElementById('nombre');
+    let apellido1 = document.getElementById('prApellido');
+    let apellido2 = document.getElementById('sgApellido');
+    let fecha = document.getElementById('fechaNacValor');
+    let email = document.getElementById('mail');
+    let user = document.getElementById('user');
+    let contraseña = document.getElementById('password');
+
+    
+})
